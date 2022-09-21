052948b76d0e40e6a076a83773cb3eb6cc8cee60
RDCERP <109607056+RDCERP@users.noreply.github.com>
2022-09-21 10:36:32 -0500

    added styling and containers

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Create a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+// Use the OpenWeather API to retrieve weather data for cities. The app will run in the browser and feature dynamically updated HTML and CSS.
+// When I view the weather dashboard, I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index.
+// When I view future weather conditions for that city, I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity.
+// When I click on a city in the search history, I am again presented with current and future conditions for that city.
+
+
+
+// Global Variables
+var city = "";
+var cityList = [];
+var cityListEl = document.querySelector("#city-list");
+var citySearchEl = document.querySelector("#city-search");
+
+
+// Functions
+
+
+
+// Event Listeners
+
+
+
